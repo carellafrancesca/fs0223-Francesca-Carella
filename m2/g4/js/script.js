c22c0c87b2305bcdc32a5054261d4c1869bc6d18
carellafrancesca <carellafrancesca29@gmail.com>
2023-04-28 18:53:25 +0200

    COMPITO-G4

---
+++ m2/g4/js/script.js
@@ -0,0 +1,21 @@
+/*
+
+let letters = document.querySelectorAll('.m-letter svg g')
+
+function selectRandom(x) {
+    return x[Math.floor(Math.random() * x.length)];
+}
+
+function lettereAnimate() {
+    let switchTrue = selectRandom(Array.from(letters));
+    if (switchTrue.classList.contains('letterM')) {
+        switchTrue.classList.remove('letterM');
+    } else {
+        switchTrue.classList.add('letterM');
+    }
+};
+setInterval(lettereAnimate, 2);
+
+*/
+
+/* MI RIMPICCIOLISCE LA BOX CON M */
