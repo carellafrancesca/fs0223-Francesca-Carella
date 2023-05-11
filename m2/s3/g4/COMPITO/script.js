3869517fc06a1d706f8681b988c638cd149ee9cb
carellafrancesca <carellafrancesca29@gmail.com>
2023-05-11 16:25:42 +0200

    COMPITO

---
+++ m2/s3/g4/COMPITO/script.js
@@ -0,0 +1,21 @@
+let apiKey = "p3fpGqWkuduCD6UXQ7E9v8NC1kbEBHRgKcZ784HmnwNGKz2KEhl6S7tN";
+
+fetch("https://api.pexels.com/v1/search?query=cities", {
+    headers: { 
+        Authorization: [apiKey],
+    },
+})
+.then((res) => {
+    console.log(res);
+    if (res.ok) {
+      return res.json();
+    } else {
+      throw new Error("Errore")
+    }
+})
+.then((data) => {
+    console.log(data.photos)
+})
+.catch((error) => {
+    console.log(error)
+})
