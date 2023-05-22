4c688aa09086b63973418497014f57aa228b72d4
Francesca <carellafrancesca29@gmail.com>
2023-05-22 15:18:57 +0200

    COMPITO

---
+++ m3/s1/g1/script.js
@@ -0,0 +1,21 @@
+var randomNumber = Math.floor(Math.random() * (100 - 1) + 1);
+console.log("Il numero generato casualmente è", randomNumber);
+var player1 = 25;
+var player2 = 60;
+var distanza_giocatore1 = Math.abs(randomNumber - player1);
+var distanza_giocatore2 = Math.abs(randomNumber - player2);
+function generateNumber() {
+    if (player1 === randomNumber) {
+        console.log("Il giocatore 1 ha azzeccato il numero casuale");
+    }
+    else if (player2 === randomNumber) {
+        console.log("Il giocatore 2 ha azzeccato il numero casuale");
+    }
+    else if (distanza_giocatore1 < distanza_giocatore2) {
+        console.log("Nessuno dei due giocatori ha azzeccato il numero casuale, ma il giocatore 1 si è avvicinato di più");
+    }
+    else {
+        console.log("Nessuno dei due giocatori ha azzeccato il numero casuale, ma il giocatore 2 si è avvicinato di più");
+    }
+}
+generateNumber();
---
+++ m3/s1/g1/script.ts
@@ -0,0 +1,22 @@
+let randomNumber:number = Math.floor(Math.random() * (100-1) + 1);
+console.log("Il numero generato casualmente è", randomNumber);
+    
+let player1:number = 35;
+let player2:number = 60;
+
+var distanza_giocatore1:number = Math.abs(randomNumber - player1);
+var distanza_giocatore2:number = Math.abs(randomNumber - player2);
+
+function generateNumber():void {
+    if (player1 === randomNumber) {
+        console.log("Il giocatore 1 ha azzeccato il numero casuale");
+    } else if (player2 === randomNumber) {
+        console.log("Il giocatore 2 ha azzeccato il numero casuale");
+    } else if (distanza_giocatore1 < distanza_giocatore2) {
+        console.log("Nessuno dei due giocatori ha azzeccato il numero casuale, ma il giocatore 1 si è avvicinato di più");
+    } else {
+        console.log("Nessuno dei due giocatori ha azzeccato il numero casuale, ma il giocatore 2 si è avvicinato di più");
+    }
+}
+
+generateNumber();
