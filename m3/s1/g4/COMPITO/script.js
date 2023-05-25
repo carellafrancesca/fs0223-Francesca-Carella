0246c8b4747046a320f236c20116b60373547806
Francesca <carellafrancesca29@gmail.com>
2023-05-25 16:32:34 +0200

    esercizi-lezione

---
+++ m3/s1/g4/COMPITO/script.js
@@ -0,0 +1,43 @@
+//CLASSE: CAPI SINGOLI
+var Capo = /** @class */ (function () {
+    function Capo(id, codprod, collezione, capo, modello, quantità, colore, prezzoivaesclusa, prezzoivainclusa, disponibile, saldo) {
+        this.id = id;
+        this.codprod = codprod;
+        this.collezione = collezione;
+        this.capo = capo;
+        this.modello = modello;
+        this.quantità = quantità;
+        this.colore = colore;
+        this.prezzoivaesclusa = prezzoivaesclusa;
+        this.prezzoivainclusa = prezzoivainclusa;
+        this.disponibile = disponibile;
+        this.saldo = saldo;
+        this.mostraPrezzo();
+    }
+    Capo.prototype.mostraPrezzo = function () {
+        console.log('Prodotto: ' + this.capo);
+        console.log('Prezzo con Iva ' + this.prezzoivainclusa + '$');
+    };
+    return Capo;
+}());
+//FETCH
+fetch("Abbigliamento.json")
+    .then(function (res) {
+    if (res.ok) {
+        return res.json();
+    }
+    else {
+        throw new Error("Errore");
+    }
+})
+    .then(function (data) {
+    var arr = [];
+    data.forEach(function (element) {
+        var capoAbbigliamento = new Capo(element.id, element.codprod, element.collezione, element.capo, element.modello, element.quantità, element.colore, element.prezzoivaesclusa, element.prezzoivainclusa, element.disponibile, element.saldo);
+        arr.push(capoAbbigliamento);
+    });
+    console.log(arr);
+})
+    .catch(function (err) {
+    console.log(err);
+});
---
+++ m3/s1/g4/COMPITO/script.ts
@@ -0,0 +1,69 @@
+//CLASSE: CAPI SINGOLI
+class Capo{
+    id:number;
+    codprod:number;
+    collezione:string;
+    capo:string;
+    modello:number;
+    quantità:number;
+    colore:string;
+    prezzoivaesclusa:number;
+    prezzoivainclusa:number;
+    disponibile:string;
+    saldo:number;
+
+    constructor(id: number, codprod: number, collezione: string, capo: string, modello: number, quantità: number, colore: string, prezzoivaesclusa: number, prezzoivainclusa: number, disponibile: string, saldo: number)
+    {
+        this.id = id;
+        this.codprod = codprod;
+        this.collezione = collezione;
+        this.capo = capo;
+        this.modello = modello;
+        this.quantità = quantità;
+        this.colore = colore;
+        this.prezzoivaesclusa = prezzoivaesclusa;
+        this.prezzoivainclusa = prezzoivainclusa;
+        this.disponibile = disponibile;
+        this.saldo = saldo;
+        this.mostraPrezzo()
+    }
+
+    mostraPrezzo(): void {
+        console.log('Prodotto: ' + this.capo);
+        console.log('Prezzo con Iva ' + this.prezzoivainclusa + '$');
+    }
+}
+
+//FETCH
+fetch("Abbigliamento.json")
+  .then((res:Response) => {
+    if (res.ok) {
+      return res.json()
+    } else {
+      throw new Error("Errore")
+    }
+  })
+  .then((data) => {
+    let arr:any = []
+    data.forEach((element:any) =>{
+    let capoAbbigliamento = new Capo(
+        element.id,
+        element.codprod,
+        element.collezione,
+        element.capo,
+        element.modello,
+        element.quantità,
+        element.colore,
+        element.prezzoivaesclusa,
+        element.prezzoivainclusa,
+        element.disponibile,
+        element.saldo
+    );
+    arr.push(capoAbbigliamento)
+    });
+    console.log(arr);
+  })
+  .catch((err) => {
+    console.log(err)
+  })
+
