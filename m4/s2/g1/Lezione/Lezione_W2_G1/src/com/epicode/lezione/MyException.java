9d425b2bbc1bae507ad8d3de902032094c0e7c4f
Francesca <carellafrancesca29@gmail.com>
2023-07-03 13:33:29 +0200

    Lezione

---
+++ m4/s2/g1/Lezione/Lezione_W2_G1/src/com/epicode/lezione/MyException.java
@@ -0,0 +1,13 @@
+package com.epicode.lezione;
+
+//Creo una claase MyException personalizzata che va a gestire con la mia logica un eventuale problema del mio codice
+//Per creare una Eccezione personalizzata devo estendere la superclasse Exception
+
+public class MyException extends Exception{
+
+	public MyException(String message) {
+		// richiamo il costruttore della superclasse Exception e gli passo un messaggio
+		super(message);
+	}
+	
+}
