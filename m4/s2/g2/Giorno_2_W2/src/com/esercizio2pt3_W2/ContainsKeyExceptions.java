9e5a806cf9da2f98266b79ef7fbb0eeee4373a01
Francesca <carellafrancesca29@gmail.com>
2023-07-04 19:28:31 +0200

    esercizi

---
+++ m4/s2/g2/Giorno_2_W2/src/com/esercizio2pt3_W2/ContainsKeyExceptions.java
@@ -0,0 +1,8 @@
+package com.esercizio2pt3_W2;
+
+public class ContainsKeyExceptions extends Exception{
+	
+	public ContainsKeyExceptions(String message) {
+		super(message);
+	}
+}
--- m4/s2/g2/Giorno_2_W2/src/com/esercizio2pt3_W2/Main_Project3.java
+++ m4/s2/g2/Giorno_2_W2/src/com/esercizio2pt3_W2/Main_Project3.java
@@ -10,13 +10,30 @@ public class Main_Project3 {
 	
 	public static void main(String[] args){
 		try {
-			aggiungiContatto("Mario", "123456789");
-			aggiungiContatto("Francesca", "123456789");
-			aggiungiContatto("Luigi", "123456789");
 			
-			rimuoviContatto("Luigi");
+			aggiungiContatto("Mario Rossi", "123456789");
+			aggiungiContatto("Luigi Verdi", "987654321");
+			aggiungiContatto("Francesca Neri", "456321789");
+			//aggiungiContatto("Mario Rossi", "123456789"); // Errore Chiave Duplicata
 			
-		}catch (Exception e) {
+			stampaContatti();
+			
+			String nomeContatto = ricercaNumeroContatto("456321789");
+			System.out.println("Nome contatto: " + nomeContatto);
+			
+			String numeroContatto = ricercaNomeContatto("Mario Rossi");
+			System.out.println("Numero contatto: " + numeroContatto);
+		
+			rimuoviContatto("Luigi Verdi");
+			// rimuoviContatto("Antonio Bianchi"); // Errore Chiave non presente in rubrica
+		
+			stampaContatti();
+			
+			System.out.println(rubrica.toString());
+		
+		} catch (ContainsKeyExceptions e) {
+			System.out.println(e.getMessage());
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
@@ -24,9 +41,10 @@ public class Main_Project3 {
 	//1
 	public static void aggiungiContatto(String nome,String telefono) throws Exception {
 		if(rubrica.containsKey(nome)) {
-			throw new Exception("Chiave già presente nella rubrica!");
+			throw new ContainsKeyExceptions("Chiave già presente nella rubrica!");
 		}else {
 			rubrica.put(nome,telefono);
+			System.out.println("Contatto aggiunto in rubrica!");
 		}
 	}	
 	
@@ -34,8 +52,9 @@ public class Main_Project3 {
 	public static void rimuoviContatto(String nome) throws Exception {
 		if(rubrica.containsKey(nome)){
 			rubrica.remove(nome);
+			System.out.println("Contatto rimosso dalla rubrica!");
 		}else {
-			throw new Exception("Chiave non presente nella rubrica");
+			throw new ContainsKeyExceptions("Chiave non presente nella rubrica");
 		}
 		
 	}
@@ -43,7 +62,8 @@ public class Main_Project3 {
 	//3
 	public static String ricercaNumeroContatto(String numero) {
 		Set <String> key = rubrica.keySet();
-		
+		//significa che viene creato un insieme (Set) di tipo String chiamato key, che conterrà le chiavi della rubrica
+		//il metodo keySet() viene chiamato sull'oggetto rubrica, che presumibilmente è di tipo Map, per ottenere l'insieme di tutte le chiavi presenti nella mappa
 		for(String k : key) {
 			if(numero.equals(rubrica.get(k))) {
 				return k;
@@ -51,17 +71,26 @@ public class Main_Project3 {
 		}	
 		return null;
 	}
+	//la funzione cerca nella rubrica un contatto che abbia il numero di telefono specificato 
+	//restituisce il nome del contatto corrispondente se viene trovato, altrimenti restituisce null
 	
 	
 	//4
 	public static String ricercaNomeContatto(String nome) {
 		return rubrica.get(nome);
 	}
+	
+	//la riga di codice return rubrica.get(nome); restituisce il valore associato alla chiave nome nella mappa rubrica
+	//se nome è una chiave valida nella mappa rubrica, il metodo get() restituirà il valore associato a quella chiave, che in questo caso dovrebbe essere una stringa rappresentante il numero di telefono corrispondente al nome
 
+	
 	//5
 	public static void stampaContatti() {
-		System.out.println("**Contatti**");
+		System.out.println("***Contatti***");
 		Set <String> key = rubrica.keySet();
+	
+		//significa che viene creato un insieme (Set) di tipo String chiamato key, che conterrà le chiavi della rubrica
+		//il metodo keySet() viene chiamato sull'oggetto rubrica, che presumibilmente è di tipo Map, per ottenere l'insieme di tutte le chiavi presenti nella mappa
 		
 		for(String k : key) {
 			System.out.println("Nome:" + "Numbero:" + rubrica.get(k));
